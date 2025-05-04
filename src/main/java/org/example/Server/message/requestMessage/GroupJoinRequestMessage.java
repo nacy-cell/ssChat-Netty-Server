d747dd05e4fe90a72bc2d9c1a9d35187cc9d26a4
lenovo <1774939767@qq.com>
2025-05-04 21:07:00 +0800

    Changes

--- src/main/java/org/example/Server/message/requestMessage/GroupJoinRequestMessage.java
+++ src/main/java/org/example/Server/message/requestMessage/GroupJoinRequestMessage.java
@@ -2,11 +2,12 @@ package org.example.Server.message.requestMessage;
 import lombok.Data;
 import lombok.ToString;
 
+import java.util.Set;
+
 @Data
 @ToString(callSuper = true)
 public class GroupJoinRequestMessage extends GroupChatRequestMessage{
     private String groupName;
-
     private Integer username;
 
     public GroupJoinRequestMessage(Integer username, String groupName) {
